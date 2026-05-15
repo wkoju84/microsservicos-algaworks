21ff065422e8774434be74f053a7d2cee7457f6b
wkoju84 <wkoju84@gmail.com>
2026-05-15 17:27:10 -0300

    commit inicial

---
+++ avaliacao/src/test/java/com/william/avaliacao/AvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
