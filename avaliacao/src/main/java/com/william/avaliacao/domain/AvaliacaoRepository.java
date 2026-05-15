e58458a658a74f21ea040725ba4cbe7a1f536144
wkoju84 <wkoju84@gmail.com>
2026-05-15 17:30:00 -0300

    adicionando AvaliacaoRepository

---
+++ avaliacao/src/main/java/com/william/avaliacao/domain/AvaliacaoRepository.java
@@ -0,0 +1,11 @@
+package com.william.avaliacao.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AvaliacaoRepository {
+
+    void save(Avaliacao avaliacao);
+    Optional<Avaliacao> getOne(Long id);
+    List<Avaliacao> getAll();
+}
